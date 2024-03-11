a81326cdcf38b6d6253fce791a0a632679757f16
Aayush181509 <aayush.181509@ncit.edu.np>
2024-03-11 22:59:43 +0545

    Factory Pattern Assignment Completed

---
+++ factory_pattern.py
@@ -0,0 +1,43 @@
+"""
+[Factory Design Pattern] Build a logging system using the Factory Design Pattern. 
+Create a LoggerFactory class that generates different types of loggers (e.g., FileLogger, ConsoleLogger, DatabaseLogger). 
+Implement methods in each logger to write logs to their respective destinations. 
+Show how the Factory Design Pattern helps to decouple the logging system from the application and allows for flexible log handling.
+"""
+
+from abc import ABC,abstractmethod
+
+class Logger(ABC):
+    def log(self,message):
+        pass
+
+class FileLogger(Logger):
+    def log(self, message):
+        with open('filelog.txt','a') as f:
+            f.write(message+'\n')
+
+class ConsoleLogger(Logger):
+    def log(self, message):
+        print(message)
+        
+
+class DatabaseLogger(Logger):
+    def log(self,message):
+        pass
+
+
+class LoggerFactory:
+    @staticmethod
+    def get_logger(l_type):
+        if l_type=='file':
+            return FileLogger()
+        elif l_type=='console':
+            return ConsoleLogger()
+        elif l_type=='database':
+            return DatabaseLogger()
+        else:
+            raise ValueError("Enter valid logger type")
+        
+        
+if __name__=="__main__":
+    LoggerFactory().get_logger('file').log("Hello!! This is Aayush")
