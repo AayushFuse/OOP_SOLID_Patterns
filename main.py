51fe0656e7d25e3bc30cb4609a475dbd0196ceea
Aayush181509 <aayush.181509@ncit.edu.np>
2024-03-12 23:22:43 +0545

    Converted every files into specific packages

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from abc import ABC,abstractmethod
+from abc import ABC, abstractmethod
 from math import pi
 
+
 class Shape(ABC):
-    def __init__(self,shape_type) -> None:
+    def __init__(self, shape_type) -> None:
         super().__init__()
         self.shape_type = shape_type
 
@@ -12,29 +13,28 @@ class Shape(ABC):
 
 
 class Circle(Shape):
-    def __init__(self,radius) -> None:
+    def __init__(self, radius) -> None:
         super().__init__("circle")
         self.radius = radius
 
     def calculate_area(self):
-        return pi*self.radius**2
-    
+        return pi * self.radius**2
+
 
 class Rectangle(Shape):
-    def __init__(self,height,width) -> None:
+    def __init__(self, height, width) -> None:
         super().__init__("rectangle")
         self.height = height
         self.width = width
 
     def calculate_area(self):
-        return self.height*self.width
-    
-    
+        return self.height * self.width
+
+
 class Square(Shape):
     def __init__(self, side) -> None:
-        super().__init__('square')
+        super().__init__("square")
         self.side = side
-    
+
     def calculate_area(self):
         return self.side**2
-    
